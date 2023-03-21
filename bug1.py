ee7a7e75615f4989d986f6d225a0545d4247932a
natalie <natalie.ky.leung@gmail.com>
2023-03-20 23:25:25 -0700

    q4 added

---
+++ bug1.py
@@ -0,0 +1,27 @@
+class Base:
+	def __init__(self, x, y, size):
+		self.x = x
+		self.y = y
+		self.size = size
+	def draw(self):
+		return f
+class Circle():
+	def __init__(x, y, size):
+		super().__init(x, y, size)
+	def draw(self):
+		return f
+class Circle({self.x}, {self.y}, {self.size})
+class Square(Base):
+	def __init__(self, y, size):
+		super().__init__(x, y, size)
+	def draw():
+		return f 
+def draw_any_shapw(myShape):
+	print(myShape.draw())
+def main():
+	s = Square(1,2,3)
+	draw_any_shape(s)
+
+	c = Circle(2,2,1)
+	draw_any_shape(c)
+main()
